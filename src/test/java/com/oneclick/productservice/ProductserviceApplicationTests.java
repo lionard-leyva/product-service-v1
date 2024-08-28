d95d2b1d976fbcf798c8a3cf22a6957864370763
leonard <lionard.cloud@gmail.com>
2024-08-28 18:04:30 +0200

    fix: test context load isolation with different contexts with docker-compose

--- src/test/java/com/oneclick/productservice/ProductserviceApplicationTests.java
+++ src/test/java/com/oneclick/productservice/ProductserviceApplicationTests.java
@@ -2,7 +2,7 @@ package com.oneclick.productservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+
 
 
 @SpringBootTest
